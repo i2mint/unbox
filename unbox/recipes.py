9e198a1f4674f97bb1a67fb001273c0aee3d6f08
Thor Whalen <thorwhalen1@gmail.com>
2021-04-08 06:32:52 -0700

    0.0.4: key_and_pattern_counts

--- unbox/recipes.py
+++ unbox/recipes.py
@@ -79,3 +79,57 @@ def print_imports_of_package(
             package, module_dotpath_filt, imported_module_dotpath_filt, depth):
         t = '\n\t'.join(imported_module_dotpaths)
         print(f"{module_dotpath}:\n\t{t}")
+
+
+########################################################################################################################
+import re
+from typing import Mapping, Union
+from types import ModuleType
+from py2store import LocalTextStore
+
+Files = Union[str, Mapping, ModuleType]
+path_sep = os.path.sep
+
+
+def get_py_files(files: Files):
+    if isinstance(files, ModuleType):
+        files = os.path.dirname(files.__file__)
+    if isinstance(files, str) and os.path.isdir(files):
+        if files.endswith(path_sep):
+            files = files[:-1]
+        files = LocalTextStore(files + path_sep + '{}.py')
+    assert isinstance(files, Mapping)
+    return files
+
+
+def key_and_pattern_counts(files: Files, pattern: Union[str, re.Pattern]):
+    """Generates (k, pattern_counts) pairs from scanning a store of .py files it scans, counting pattern matches
+
+    :param files: A source of py files (e.g. module, root directory, or a Mapping itself)
+    :param pattern: A string or re.Pattern to match and count
+    :return: A generator of (k, pattern_counts) pairs
+
+    In order to be able to replicate this test we'll mention a word that has little chance of showing up in an
+    uncontrolled matter in this this unbox package. This word: supercalifragilisticexpialidocious
+    It should appear only twice in this current recipes.py -- once above, and once when we ask for
+    key_and_pattern_counts to match it.
+
+    Now, let's see:
+
+    >>> import unbox
+    >>> sorted(key_and_pattern_counts(unbox, 'supercalifragilisticexpialidocious'))
+    [('__init__.py', 0), ('base.py', 0), ('recipes.py', 2)]
+
+    """
+    files = get_py_files(files)
+    if isinstance(pattern, str):
+        pattern = re.compile(pattern)
+    for k, contents in files.items():
+        pattern_count = len(pattern.findall(contents))
+        yield k, pattern_count
+
+
+def print_py_files_containing_pattern(files: Files, pattern: Union[str, re.Pattern]):
+    for key, pattern_count in key_and_pattern_counts(files, pattern):
+        if pattern_count > 0:
+            print(f"{key}: {pattern_count}")
