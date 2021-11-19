048965f6995a70f6c6096c2429651c050d58b4d7
GitHub CI Runner <thorwhalen1@gmail.com>
2021-11-19 23:47:56 +0000

    **CI** Formatted code + Updated version number and documentation. [skip ci]

--- unbox/missing_install_names.py
+++ unbox/missing_install_names.py
@@ -293,5 +293,6 @@ def module_requirements_according_to_setupcfg(module):
     return list(filter(None, s['options']['install_requires'].split('\n')))
 
 
-install_requires_of_module = module_requirements_according_to_setupcfg  # backcompa alias
-
+install_requires_of_module = (
+    module_requirements_according_to_setupcfg  # backcompa alias
+)
