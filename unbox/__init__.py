70d286ab3cadd78cb8a9b3fd35e58ec30d66831a
Thor Whalen <thorwhalen1@gmail.com>
2021-12-03 18:14:31 -0800

    fix: Removed non existing module_to_setup_cfg_filepath

--- unbox/__init__.py
+++ unbox/__init__.py
@@ -4,7 +4,6 @@ from unbox.base import builtin_module_names
 from unbox.missing_install_names import (
     dependency_diff_for_pkg,
     print_missing_names,
-    module_to_setup_cfg_filepath,
     module_requirements_according_to_setupcfg,
     install_requires_of_module,  # deprecate!
 )
