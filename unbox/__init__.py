4fe1e4ffed3d57ff57be97b4ef2ccd979eeba288
Thor Whalen <thorwhalen1@gmail.com>
2022-01-05 12:50:19 -0800

    feat: get_import_names and install_names_for_imports exposed in root + some module docs

--- unbox/__init__.py
+++ unbox/__init__.py
@@ -1,7 +1,22 @@
-"""Finding imports in code"""
+"""Finding imports in code etc.
+
+``get_import_names``: Getting the names that are imported.
+
+``install_names_for_imports``: pip_install name for given import name (if different
+and listed in our data)
+
+``module_requirements_according_to_setupcfg``: Getting deps from setup.cfg file
+
+``print_missing_names``: Print the difference between required names and imported (
+install) names
+
+
+"""
 from unbox.base import *
 from unbox.base import builtin_module_names
 from unbox.missing_install_names import (
+    get_import_names,
+    install_names_for_imports,
     dependency_diff_for_pkg,
     print_missing_names,
     module_requirements_according_to_setupcfg,
